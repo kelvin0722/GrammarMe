e43e0bf3807eaa79d73e2d80a6b0b2644b7e18b1
kelvin0722 <kelvinm442@gmail.com>
2026-09-02 19:34:13 +0300

    Initial Commit

---
+++ GrammarMe/GrammarMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrammarMeApp.swift
+//  GrammarMe
+//
+//  Created by Kelvin Ngacha on 02/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct GrammarMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
